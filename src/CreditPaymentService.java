e22f852e6e814f356596bb018a408b0eb0df87fc
Elena Levina <qa-levina@yandex.ru>
2022-06-23 13:22:30 +0300

    Отформатировала код

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,9 @@
 public class CreditPaymentService {
 
     public double calculate(double s, double n, double i) {
-
         double p = i / 100 / 12;
         double p2 = 1 + p;
-
         double index = s * (p + (p / (Math.pow(p2, n) - 1)));
-
         return index;
 
     }
