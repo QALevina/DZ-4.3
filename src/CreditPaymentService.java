7d03407550ec2b44c8c53ddd278aa82ed1edc4f6
Elena Levina <qa-levina@yandex.ru>
2022-06-23 17:36:30 +0300

    Параметры назвала осмысленно

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
 
-    public double calculate(double s, double n, double i) {
-        double p = i / 100 / 12;
-        double p2 = 1 + p;
-        double index = s * (p + (p / (Math.pow(p2, n) - 1)));
+    public double calculate(double creditAmount, double loanTerm, double interestRate) {
+        double monthlyInterestRate = interestRate / 100 / 12;
+        double exponentiationMonthlyInterestRate = 1 + monthlyInterestRate;
+        double index = creditAmount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(exponentiationMonthlyInterestRate, loanTerm) - 1)));
         return index;
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.concurrent.Callable;
-
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
